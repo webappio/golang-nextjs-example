eb82f2aa2fa5ab8d7aabb84aab84873237bc3a79
Colin Chartier <colin@webapp.io>
2022-12-13 23:26:40 -0500

    Switch things to use a websocket

--- services/backend/main.go
+++ services/backend/main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		var resp []byte
-		if req.URL.Path == "/status" {
-			resp = []byte(`{"status": "ok"}`)
-		} else if req.URL.Path == "/username" {
-			resp = []byte(`{"username": "colin"}`)
+		if req.URL.Path == "/handler-initial-data" {
+			resp = []byte(`{"text": "initial"}`)
+		} else if req.URL.Path == "/handler" {
+			conn, _, _, err := ws.UpgradeHTTP(req, rw)
+			if err != nil {
+				log.Println("Error with WebSocket: ", err)
+				rw.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
+			go func() {
+				defer conn.Close()
+
+				time.Sleep(time.Second) //TODO HACK: sleep a second to check everything is working properly
+				err = wsutil.WriteServerMessage(conn, ws.OpText, []byte(`{"text": "from-websocket"}`))
+				if err != nil {
+					log.Println("Error writing WebSocket data: ", err)
+					return
+				}
+			}()
+			return
 		} else {
 			rw.WriteHeader(http.StatusNotFound)
 			return
